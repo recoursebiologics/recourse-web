63c2400b8c7ed21ef26a8cc8c33a6a1b157ee9d0
Jackson Thetford <j.thetford@utexas.edu>
2025-06-24 15:37:53 -0500

    updating for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/recourse-precision-web/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
